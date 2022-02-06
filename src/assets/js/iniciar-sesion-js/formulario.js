929545387509596271c3cb531d99db36c2b26454
Marja <castanedamarjaQgmail.com>
2022-02-06 11:25:26 -0600

    se cambio el formato de registro e iniciar sesión a dos documentos diferentes

--- src/assets/js/iniciar-sesion-js/formulario.js
+++ src/assets/js/iniciar-sesion-js/formulario.js
@@ -2,22 +2,15 @@ const formulario = document.getElementById('formulario');
 const inputs = document.querySelectorAll('#formulario input')
 
 const expresiones = {
-	apellido: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
-	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
 	password: /^.{4,12}$/, // 4 a 12 digitos.
 	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-	telefono: /^\d{7,14}$/ // 7 a 14 numeros.
+	
 }
 
 const campos ={
 	password: false,
 	correo:  false ,
-    apellido: false,
-    nombre:  false,
-    telefono: false,
-    password2: false,
-    correo2:  false, 
-
+   
 
 }
 
