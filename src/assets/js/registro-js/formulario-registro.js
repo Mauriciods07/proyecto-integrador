38f99ae78c7cab493dbcb4df4249bdb52cd0cc1e
Marja <castanedamarjaQgmail.com>
2022-02-08 18:44:45 -0600

    formulario registro en buen estado

--- src/assets/js/registro-js/formulario-registro.js
+++ src/assets/js/registro-js/formulario-registro.js
@@ -4,15 +4,15 @@ const inputs= document.querySelectorAll ('#formulario input')
 const expresiones = {
 	
 	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
-	password: /^.{4,12}$/, // 4 a 12 digitos.
+    apellido: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
 	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+    password: /^.{4,12}$/, // 4 a 12 digitos.
 	telefono: /^\d{10,11}$/ // 10 a 11 numeros.
 }
 const campos ={
-	password: false,
-	correo:  false ,
     nombre: false,
-    correo: false,
+    apellido:false,
+	correo:  false ,
     password: false,
     telefono: false
    
@@ -24,7 +24,7 @@ const validarFormulario = (e) =>{
           validarCampo (expresiones.nombre, e.target, 'nombre');
      break;
     case  "apellido":
-        validarCampo (expresiones.nombre, e.target, 'apellido');
+        validarCampo (expresiones.apellido, e.target, 'apellido');
     
     break;
     case  "correo":
@@ -44,12 +44,13 @@ const validarFormulario = (e) =>{
 
 const validarCampo = (expresion , input, campo ) =>{
 
-    if (expresion.test (input.value))    {
+    if (expresion.test (input.value)){
         document.getElementById (`grupo-${campo}`).classList.remove ('formulario_grupo-incorrecto');
         document.getElementById (`grupo-${campo}`).classList.add ('formulario_grupo-correcto');
         document.querySelector(`#grupo-${campo} i`).classList.add('fa-check-circle');
         document.querySelector(`#grupo-${campo} i`).classList.remove('fa-times-circle');
         document.querySelector(`#grupo-${campo} .formulario_input-error`).classList.remove('formulario_input-error-activo')
+        campos [campo] = true;
 
     } else {
         document.getElementById (`grupo-${campo}`).classList.add ('formulario_grupo-incorrecto')
@@ -57,7 +58,9 @@ const validarCampo = (expresion , input, campo ) =>{
         document.querySelector(`#grupo-${campo} i`).classList.add('fa-times-circle');
         document.querySelector(`#grupo-${campo} i`).classList.remove('fa-check-circle');
         document.querySelector(`#grupo-${campo} .formulario_input-error`).classList.add('formulario_input-error-activo')
+        campos [campo] = false;
     }
+
     
 }
 
@@ -70,7 +73,7 @@ inputs.forEach((input) =>{
 
 formulario.addEventListener ('submit', (e) =>{
 
-    if (campos.nombre &&  campos.apellido && campos.correo && campos.password &&  campos.telefono){
+    if (campos.nombre && campos.apellido && campos.correo && campos.password && campos.telefono){
         window.location.href="usuario-perfil.html";
       formulario.reset();
         document.querySelectorAll('.formulario_grupo-correcto').forEach ((icono) =>{
