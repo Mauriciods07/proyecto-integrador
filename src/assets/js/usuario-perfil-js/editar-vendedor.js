603a92cb5e5d4e16bc9b16a22cec81baf27001e2
Jonathan Quino <jonathan.quino.26793@gmail.com>
2022-02-18 09:48:39 -0600

    Se quita el apartado de Nombre legal de vendedor

--- src/assets/js/usuario-perfil-js/editar-vendedor.js
+++ src/assets/js/usuario-perfil-js/editar-vendedor.js
@@ -1,6 +1,6 @@
 $btnEditarVendedor.addEventListener('click',(e) =>{ //Funcion donde, el click a boton editar vendedor, habilita los campos inputs, botones de agregar producto y guardar vendedor. Mientras que el boton editar vendedor se deshabilita
     $inputNombreComercio.removeAttribute('disabled');
-    $inputNombreVendedor.removeAttribute('disabled');
+    //$inputNombreVendedor.removeAttribute('disabled');
     $inputCorreoEmpresa.removeAttribute('disabled');
     $inputTelefonoEmpresa.removeAttribute('disabled');
     $inputDireccionEmpresa.removeAttribute('disabled');
