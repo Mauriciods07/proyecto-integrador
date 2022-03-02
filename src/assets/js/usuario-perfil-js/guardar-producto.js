1ec30167de958ac4b4281bf061b57fc4421f643c
Jonathan Quino <jonathan.quino.26793@gmail.com>
2022-03-02 07:49:27 -0600

    Parte de usuario lista

--- src/assets/js/usuario-perfil-js/guardar-producto.js
+++ src/assets/js/usuario-perfil-js/guardar-producto.js
@@ -1,10 +1,37 @@
 $btnModalAceptarProductoNuevo.addEventListener('click', (e) =>{ //Boton donde el dar click, verifica que las banderas de los inputs dentro del modal esten correctamente ingresadas, si lo estan, se agrega el producto(REQUIERE API)
     let ruta = $imgProductoNuevo.src;
     if(nombreproducto_OK && descripcionproducto_OK && costoproducto_OK && cantidadproducto_OK && !ruta.includes("error.png")){
+
         e.target.setAttribute('data-dismiss',"modal");
-        alert('Agregado Exitoso');
+        const urlNuevoProducto = 'http://localhost:8080/Productos/nuevoProducto'
+        fetch(urlNuevoProducto, {
+            method : 'POST',
+            body: JSON.stringify({usuario:{
+                                          id:localStorage.getItem('id')
+                                          },
+                                 nombreProducto:$inputNombreProductoNuevo.value,
+                                 descripcion:$inputDescripcionProductoNuevo.value,
+                                 imagen:"ImagendeProductoSubido",
+                                 costo:$inputCostoProductoNuevo.value,
+                                 cantidad:$inputCantidadProductoNuevo.value,
+                                 categoria:$inputCategoriaProductoNuevo.value,
+                                 fechaProducto:fechaActual
+            }),
+            headers: {
+                "Content-Type": "application/json; charset = UTF-8"
+            }
+        }).then(res =>{
+            if(res.ok){
+                alert("Se guardo el producto correctamente")
+            }
+            else{
+                alert("Hubo un error al guardar el producto")
+            }
+        })
     }
     else{
+        
+        
         alert('Revisa los apartados en rojo y recuerda que debes subir una imagen de tu producto');
         document.querySelector('#formularioProducto').addEventListener('submit',(e)=>{
             e.preventDefault();
