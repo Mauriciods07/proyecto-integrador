a8d10a293a9b7de7c34383340a5027503fea0905
Nayelli Gutierrez <nayelli.gutierrez.perez@gmail.com>
2022-03-03 04:40:40 -0600

    Fetch de login

--- src/assets/js/iniciar-sesion-js/isLogged.js
+++
@@ -1,7 +0,0 @@
-const token = localStorage.getItem('token');
-
-if(!token) {
-    url = window.location;
-    const path = url.pathname.substring(0, url.pathname.lastIndexOf('/') + 1)
-    location.href = path +  'iniciar-sesion.html';
-}
